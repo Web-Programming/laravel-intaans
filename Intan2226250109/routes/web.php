67525ba37fa7dc63eea9bc87719b542219992aaa
IntanSanu <intansanu_2226250109@mhs.mdp.ac.id>
2023-10-09 13:09:07 +0700

    IntanSanu2226250109

--- Intan2226250109/routes/web.php
+++ Intan2226250109/routes/web.php
@@ -16,3 +16,44 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// buat route ke halaman profil
+Route::get('/', function () {
+    return view('profil');
+});
+
+//route dengan parameter (wajib)
+Route::get("/mahasiswa/{nama}", function ($nama = "Intans") {
+   echo "<h1> Halo Nama Saya $nama</h1>";
+});
+
+//route dengan parameter (tidak wajib)
+Route::get("/mahasiswa2/{nama?}", function ($nama = "Intans") {
+   echo "<h1> Halo Nama Saya $nama</h1>";
+});
+
+//route dengan parameter >1
+Route::get("/profil/{nama}/{pekerjaan}", function ($nama = "Intans", $pekerjaan = "Mahasiswa") {
+   echo "<h1> Halo Nama Saya $nama, saya adalah $pekerjaan</h1>";
+});
+
+//Redirect
+Route::get("/hubungi", function () {
+    echo "<h1>Hubungi Kami</h1>";
+})->name("call"); //named route
+
+Route::redirect("/contact", "/hubungi");
+
+Route::get("/halo", function () {
+    echo "<a href='".route('call'). "'>".route('call')."</a>";
+});
+
+Route::prefix("/dosen")->group(function(){
+    Route::get("/jadwal", function(){
+        echo "<h1> Jadwal Dosen </h1>";
+    });
+     Route::get("/materi", function(){
+        echo "<h1> Materi Perkuliahan </h1>";
+    });
+    //dan lain-lain
+});
